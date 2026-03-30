62a01631b7fd3311c923f8bb96dd80c9ee96e623
saeid721 <saeid721km@gmail.com>
2026-03-30 09:54:47 +0600

    update

--- New folder/src/features/categories/Categories.jsx
+++
@@ -1,4 +0,0 @@
-
-export { default as CategoryCard } from "./components/CategoryCard";
-export { default as CategoryGrid } from "./components/CategoryGrid";
-export { default as CategoryList } from "./data/CategoryList";
